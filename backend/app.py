790201daaacfb51457630ec6554006402f4bbc2d
Ziad Mahmoud Mostafa <99136886+ziad-mahmoud22@users.noreply.github.com>
2025-06-23 14:23:55 +0300

    removed js code and files , added app_new.py

--- backend/app.py
+++ backend/app.py
@@ -149,4 +149,4 @@ def health_check():
     return jsonify({"status": "Server is running"}), 200
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=8001)
+    app.run(debug=True, host='0.0.0.0', port=5000)
